73c05b362f18b7c02bba549494909ef509b73bd8
shpilson <kovalenko.jora@gmail.com>
2021-06-28 15:47:24 +0200

    update

--- scripts/pages/Main/blocks/Advantages/Advantages.js
+++ scripts/pages/Main/blocks/Advantages/Advantages.js
@@ -1,12 +1,12 @@
 const Advantages = () => {
     $('.page-section .container .section-title').prepend(
-        '<img src="https://shpilson.github.io/smoke/images/prefix.png" width="32px" class="leave">',
+        '<img src="https://shpilson.github.io/smoke/images/prefix.svg" width="32px" class="leave">',
     );
 
     $('.custom_section').append(
         `<div class="advantages-container container">
         <h4 class="advantage-title">
-          <img src="https://shpilson.github.io/smoke/images/prefix.png" width="32px" class="leave">
+          <img src="https://shpilson.github.io/smoke/images/prefix.svg" width="32px" class="leave">
           Преимущества
         </h4>
         <ul class="advantages-list">
@@ -14,10 +14,10 @@ const Advantages = () => {
             <img src="https://shpilson.github.io/smoke/images/1adv.svg">
             <p>В розницу и оптом по Мск и всей России</p></li>
           <li class="advantages-list-item">
-            <img src="https://shpilson.github.io/smoke/images/2adv.png">
+            <img src="https://shpilson.github.io/smoke/images/2adv.svg">
             <p>Индивидуальный подход  к каждому клиенту</p></li>
           <li class="advantages-list-item">
-            <img src="https://shpilson.github.io/smoke/images/3adv.png">
+            <img src="https://shpilson.github.io/smoke/images/3adv.svg">
             <p>Исполнение Ваших пожеланий  - наш успех </p></li>
         </ul>
       </div>`,
