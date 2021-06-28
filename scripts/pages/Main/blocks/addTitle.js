b01b0828a93eb2087a848b91b83ca020aacf3306
shpilson <kovalenko.jora@gmail.com>
2021-06-28 16:40:38 +0200

    update

--- scripts/pages/Main/blocks/addTitle.js
+++ scripts/pages/Main/blocks/addTitle.js
@@ -1,6 +1,6 @@
 const addTitle = (element, title) => {
     element.insertAdjacentHTML('afterbegin', `
-    <h3 class="w-100 container section-title mb-4 mt-4"><img src="https://shpilson.github.io/smoke/images/prefix.svg" width="32px" class="leave"><span>${title}</span></h3>`)
+    <h3 class="w-100 container section-title mb-4 mt-4"><span>${title}</span></h3>`)
   }
   
   export default addTitle;
