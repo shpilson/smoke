0e51f9b84962ac5107c5340fa5e31eff6b680599
shpilson <kovalenko.jora@gmail.com>
2021-06-28 15:45:04 +0200

    update

--- scripts/pages/Main/blocks/Advantages/Advantages.js
+++ scripts/pages/Main/blocks/Advantages/Advantages.js
@@ -11,7 +11,7 @@ const Advantages = () => {
         </h4>
         <ul class="advantages-list">
           <li class="advantages-list-item">
-            <img src="https://shpilson.github.io/smoke/images/1adv.png">
+            <img src="https://shpilson.github.io/smoke/images/1adv.svg">
             <p>В розницу и оптом по Мск и всей России</p></li>
           <li class="advantages-list-item">
             <img src="https://shpilson.github.io/smoke/images/2adv.png">
