4e7da1b5451beefe98f4493495ff469d4fd8402e
shpilson <kovalenko.jora@gmail.com>
2021-06-28 15:32:05 +0200

    update

---
+++ scripts/pages/Main/blocks/Advantages/Advantages.js
@@ -0,0 +1,31 @@
+const Advantages = () => {
+    $('.page-section .container .section-title').prepend(
+        '<img src="https://res.cloudinary.com/rostislavsellavi/image/upload/v1623852929/shops/108750/IMG_2004_2-removebg-preview_1_mftrk3.png" width="32px" class="leave">',
+    );
+
+    // $('.homefeatured_category .container .section-title').prepend(
+    //   '<img src="https://res.cloudinary.com/rostislavsellavi/image/upload/v1623852929/shops/108750/IMG_2004_2-removebg-preview_1_mftrk3.png" width="32px" class="leave">'
+    // )
+
+    $('.custom_section').append(
+        `<div class="advantages-container container">
+        <h4 class="advantage-title">
+          <img src="https://res.cloudinary.com/rostislavsellavi/image/upload/v1623852929/shops/108750/IMG_2004_2-removebg-preview_1_mftrk3.png" width="32px" class="leave">
+          Преимущества
+        </h4>
+        <ul class="advantages-list">
+          <li class="advantages-list-item">
+            <img src="https://res.cloudinary.com/rostislavsellavi/image/upload/v1624202051/shops/108750/Group_7423954_1_y0xdaw.png">
+            <p>В розницу и оптом по Мск и всей России</p></li>
+          <li class="advantages-list-item">
+            <img src="https://res.cloudinary.com/rostislavsellavi/image/upload/v1624202051/shops/108750/Group_7423955_1_hiwduo.png">
+            <p>Индивидуальный подход  к каждому клиенту</p></li>
+          <li class="advantages-list-item">
+            <img src="https://res.cloudinary.com/rostislavsellavi/image/upload/v1624202051/shops/108750/Group_7423956_1_d8idll.png">
+            <p>Исполнение Ваших пожеланий  - наш успех </p></li>
+        </ul>
+      </div>`,
+    );
+};
+
+export default Advantages;
