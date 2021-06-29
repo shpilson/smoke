ae376dcdeb2c726ee457fce7c78c18ab3ed31619
shpilson <kovalenko.jora@gmail.com>
2021-06-29 11:55:39 +0200

    update

--- scripts/Menu/Menu.js
+++ scripts/Menu/Menu.js
@@ -135,16 +135,10 @@ const constants = {
 
 import openMenu from './scripts/openMenu.js';
 import createBurger from './scripts/createBurger.js';
-import setMenuContainer from './scripts/setMenuContainer.js';
-import renderLevels from './scripts/renderLevels.js';
-import showSubMenu from './scripts/showSubMenu.js';
 
 const activateMenu = () => {
-  // setMenuContainer(constants.elements.header.navigation.wrapper())
   createBurger(constants.elements.header.wrapper.logo());
   openMenu();
-  // renderLevels(constants.elements.header.navigation.levels);
-  // showSubMenu();
 }
 
 activateMenu();
